787c0dc971fdc5f640181f55dfd15959d11da3ac
santosh rai <santoshrai2072001@gmail.com>
2024-01-01 17:53:45 +0530

    first commit using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='swiggy top 3 recommendation tempelate',
+    author='santosh',
+    license='',
+)
